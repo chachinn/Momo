d1eb2ee94e62869439945bf8398b5563bb93a15e
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-09 12:26:33 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 // ========================================
 // MOMO SERVICE WORKER
-// Momo 1.3.1 — stable network-first PWA shell
+// Momo 1.3.2 — stable network-first PWA shell
 // ========================================
 
 const CACHE_NAME =
-  "momo-runtime-shell-v1.3.1";
+  "momo-runtime-shell-v1.3.2";
 
 
 const APP_SHELL = [
